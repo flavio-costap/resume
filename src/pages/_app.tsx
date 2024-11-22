229c6dc2996cba7f0e6183ce3a58a3693752e198
Shiryu <fgc.shiryu@gmail.com>
2024-11-21 22:08:45 -0300

    first commit initial structure
    Signed-off-by: Shiryu <fgc.shiryu@gmail.com>

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import "../styles/globals.css";
+import localFont from "next/font/local";
+import { AppProps } from "next/app";
+
+const geistSans = localFont({
+  src: "../../public/fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../../public/fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Component {...pageProps} />
+      </body>
+    </html>
+  );
+}
