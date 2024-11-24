49c13e8412e97732fd457fa03112133766536233
Shiryu <fgc.shiryu@gmail.com>
2024-11-24 11:16:10 -0300

    updating SEO
    Signed-off-by: Shiryu <fgc.shiryu@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,6 +21,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Flávio Costa</title>
         <meta name="description" content="Esse é meu currículo interativo, fique a vontade para entrar em contato comigo" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Flávio Costa Engenharia de Software"></meta>
+        <meta property="og:site_name" content="flavio-costap"></meta>
+        <meta property="og:description" content="Site currículo de um especialista em front-end" />
+        <meta property="og:url" content="https://flaviocosta-eng.vercel.app/" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
