be61bdeef7c5c9a464c910bd9873bac4224001e3
Shiryu <fgc.shiryu@gmail.com>
2025-03-19 09:36:04 -0300

    adjusting sublist
    Signed-off-by: Shiryu <fgc.shiryu@gmail.com>

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -9,12 +9,12 @@ function AboutSection() {
       title: "Sobre mim",
       paragraph1: (
         <>
-          Sou engenheiro de software formado em Engenharia de Software pela Universidade de Brasília (UnB) e atuo na área de desenvolvimento web desde 2018. Especializado em <b className="text-blue-500">front-end</b>, possuo expertise em tecnologias modernas como <b className="text-blue-500">React</b> e <b className="text-blue-500">Vue</b>, além de conhecimento em Node, Docker, integração contínua, bancos de dados e testes automatizados. Minha experiência inclui a criação de interfaces responsivas, intuitivas e escaláveis, sempre focadas em oferecer a melhor experiência para o usuário e atender às necessidades dos projetos de maneira eficiente e inovadora.
+          Sou engenheiro de software formado em Engenharia de Software pela Universidade de Brasília (UnB) e atuo na área de desenvolvimento web desde 2018. Especializado em <b className="text-blue-500">front-end</b>, possuo expertise em tecnologias modernas como <b className="text-blue-500">React</b>, além de conhecimento em Node, Docker, integração contínua, bancos de dados e testes automatizados. Minha experiência inclui a criação de interfaces responsivas, intuitivas e escaláveis, sempre focadas em oferecer a melhor experiência para o usuário e atender às necessidades dos projetos de maneira eficiente e inovadora.
         </>
       ),
       paragraph2: (
         <>
-          Ao longo da minha carreira, participei de projetos de destaque para clientes renomados, como <b className="text-blue-500">Samsung</b> e <b className="text-blue-500">Carrefour</b>, contribuindo com soluções de alto impacto. Além disso, desenvolvi sistemas para diversas áreas, como rastreamento veicular, gerenciamento fiscal e marketing digital. Também busco constantemente me aperfeiçoar, o que me levou a conquistar algumas certificações consolidando minha capacidade de entrega e alinhamento com as <b className="text-blue-500">melhores práticas</b> do mercado.
+          Ao longo da minha carreira, participei de projetos de destaque para clientes renomados, contribuindo com soluções de alto impacto. Além disso, desenvolvi sistemas para diversas áreas, como rastreamento veicular, gerenciamento fiscal e marketing digital. Também busco constantemente me aperfeiçoar, o que me levou a conquistar algumas certificações consolidando minha capacidade de entrega e alinhamento com as <b className="text-blue-500">melhores práticas</b> do mercado.
         </>
       )
     },
@@ -22,12 +22,12 @@ function AboutSection() {
       title: "About me",
       paragraph1: (
         <>
-          I am a software engineer graduated in Software Engineering from the University of Brasília (UnB) and have been working in web development since 2018. Specialized in <b className="text-blue-500">front-end</b>, I have expertise in modern technologies such as <b className="text-blue-500">React</b> and <b className="text-blue-500">Vue</b>, along with knowledge in Node, Docker, continuous integration, databases, and automated testing. My experience includes creating responsive, intuitive, and scalable interfaces, always focused on providing the best user experience and efficiently meeting project needs in an innovative way.
+          I am a software engineer graduated in Software Engineering from the University of Brasília (UnB) and have been working in web development since 2018. Specialized in <b className="text-blue-500">front-end</b>, I have expertise in modern technologies such as <b className="text-blue-500">React</b>, along with knowledge in Node, Docker, continuous integration, databases, and automated testing. My experience includes creating responsive, intuitive, and scalable interfaces, always focused on providing the best user experience and efficiently meeting project needs in an innovative way.
         </>
       ),
       paragraph2: (
         <>
-          Throughout my career, I have participated in high-profile projects for renowned clients such as <b className="text-blue-500">Samsung</b> and <b className="text-blue-500">Carrefour</b>, contributing with high-impact solutions. I have also developed systems for various areas, such as vehicle tracking, fiscal management, and digital marketing. I am also constantly striving to improve myself, which has led me to achieve several certifications, consolidating my ability to deliver and align with the <b className="text-blue-500">best practices</b> in the market.
+          Throughout my career, I have participated in high-profile projects for renowned clients contributing with high-impact solutions. I have also developed systems for various areas, such as vehicle tracking, fiscal management, and digital marketing. I am also constantly striving to improve myself, which has led me to achieve several certifications, consolidating my ability to deliver and align with the <b className="text-blue-500">best practices</b> in the market.
         </>
       )
     }
